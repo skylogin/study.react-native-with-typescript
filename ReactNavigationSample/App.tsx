51d5fbb71543bf261754f275dece503b154a7d9c
skylogin <skylogin@gmail.com>
2021-07-19 10:39:19 +0900

    react-navigation 기초스택 및 테마적용

--- ReactNavigationSample/App.tsx
+++ ReactNavigationSample/App.tsx
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
+import 'react-native-gesture-handler';
+import {enableScreens} from 'react-native-screens';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
+
 import MainNavigator from './src/screens/MainNavigator';
 
+import {ToggleThemeProvider} from './src/contexts';
+
+enableScreens();
+
 export default function App() {
-  return <MainNavigator />;
+  const scheme = useColorScheme();
+  const [theme, setTheme] = useState(
+    scheme === 'dark' ? DarkTheme : DefaultTheme,
+  );
+
+  const toggleTheme = useCallback(
+    () => setTheme(({dark}) => (dark ? DefaultTheme : DarkTheme)),
+    [],
+  );
+
+  return (
+    <AppearanceProvider>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaProvider>
+          <NavigationContainer theme={theme}>
+            <MainNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ToggleThemeProvider>
+    </AppearanceProvider>
+  );
 }
--- ReactNavigationSample/src/screens/MainNavigator.tsx
+++ ReactNavigationSample/src/screens/MainNavigator.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Switch} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useTheme} from '@react-navigation/native';
+import {useToggleTheme} from '../contexts';
 
 export default function MainNavigator() {
+  const theme = useTheme();
+  const toggleTheme = useToggleTheme();
+
   return (
-    <View style={[styles.view]}>
-      <Text>This is Top text</Text>
-      <Text>This is Bottom text</Text>
-    </View>
+    <SafeAreaView style={[styles.flex]}>
+      <View style={[styles.view, {backgroundColor: theme.colors.background}]}>
+        <View style={[styles.topBar, {backgroundColor: theme.colors.primary}]}>
+          <Switch value={theme.dark} onValueChange={toggleTheme} />
+        </View>
+        <View style={[styles.view]}>
+          <Text style={[styles.text, {color: theme.colors.text}]}>
+            This is Top text
+          </Text>
+          <Text style={[styles.text, {color: theme.colors.text}]}>
+            This is Bottom text
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
   view: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  topBar: {
+    width: '100%',
+    flexDirection: 'row',
+    padding: 5,
+    justifyContent: 'flex-end',
+  },
+  text: {
+    fontSize: 20,
   },
 });
