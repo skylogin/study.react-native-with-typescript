432f2bed06c5b2c6ea0cb6c993c2ec03a5ca0239
skylogin <skylogin@gmail.com>
2021-07-19 11:27:25 +0900

    관련 테마컴포넌트 추가

---
+++ ReactNavigationSample/src/theme/navigation/Text.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import type {FC, ComponentProps} from 'react';
+import {StyleSheet, Text as RNText} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+
+export type TextProps = ComponentProps<typeof RNText>;
+
+export const Text: FC<TextProps> = ({style, ...props}) => {
+  const {colors} = useTheme();
+  return (
+    <RNText
+      style={[
+        styles.underline,
+        {color: colors.text, textDecorationColor: colors.text},
+        style,
+      ]}
+      {...props}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  underline: {
+    textDecorationLine: 'underline',
+  },
+});
