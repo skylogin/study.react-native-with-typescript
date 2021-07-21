19dbb0b93b5329264d8dd08964e154c7a3890503
skylogin <skylogin@gmail.com>
2021-07-21 16:20:53 +0900

    useReducer 훅 사용

--- ReactReduxSample/src/screens/UseReducer.tsx
+++ ReactReduxSample/src/screens/UseReducer.tsx
@@ -1,32 +1,58 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {SafeAreaView, View, Text, TopBar} from '../theme/navigation';
+import React, {useReducer, useEffect} from 'react';
+import {StyleSheet, FlatList} from 'react-native';
+import {SafeAreaView, View, UnderlineText, TopBar} from '../theme/navigation';
+import {ScrollEnabledProvider, useScrollEnabled} from '../contexts';
+import * as D from '../data';
+import Person from './Person';
+import * as P from '../store/people';
 
-const title = 'CopyMe';
-export default function CopyMe() {
+export default function UseReducer() {
+  const [scrollEnabled] = useScrollEnabled();
+  const [people, dispatch] = useReducer(
+    (state: D.IPerson[], action: P.Actions) => {
+      switch (action.type) {
+        case '@person/add':
+          return [...state, action.payload];
+        case '@person/delete':
+          return state.filter(person => person.id !== action.payload.id);
+        case '@person/deleteAll':
+          return [];
+      }
+      return state; // 반드시 있을 필요는 없습니다.
+    },
+    [],
+  );
+  const addPerson = () => dispatch(P.addAction(D.createRandomPerson()));
+  const removeAllPersons = () => dispatch(P.deleteAllAction());
+  const deletePerson = (id: string) => () => dispatch(P.deleteAction(id));
+  useEffect(addPerson, []);
+
   return (
     <SafeAreaView>
-      <View style={[styles.view]}>
-        <TopBar />
-        <View style={[styles.content]}>
-          <Text style={[styles.text]}>{title}</Text>
+      <ScrollEnabledProvider>
+        <View style={[styles.view]}>
+          <TopBar>
+            <UnderlineText onPress={addPerson} style={styles.text}>
+              add
+            </UnderlineText>
+            <UnderlineText onPress={removeAllPersons} style={styles.text}>
+              remove all
+            </UnderlineText>
+          </TopBar>
+          <FlatList
+            scrollEnabled={scrollEnabled}
+            data={people}
+            renderItem={({item}) => (
+              <Person person={item} deletePressed={deletePerson(item.id)} />
+            )}
+            keyExtractor={item => item.id}
+          />
         </View>
-      </View>
+      </ScrollEnabledProvider>
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    padding: 5,
-  },
-  text: {
-    fontSize: 20,
-  },
-  content: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  view: {flex: 1},
+  text: {marginRight: 10, fontSize: 20},
 });
